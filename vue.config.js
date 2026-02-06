7af1d93fea6e3e13d849517d39de6c9ff60be1e1
Roxana <ms.roxanarahimi@gmail.com>
2026-02-06 22:32:28 +0330

    shop

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa:{
-    themeColor: 'var(--bs-primary)',
-    msTileColor: 'var(--bs-primary)',
+    themeColor: '#E60076',//'var(--bs-primary)',
+    msTileColor: '#E60076',//'var(--bs-primary)',
     appleMobileWebAppStatusBarStyle: 'black-translucent',
   }
 })
