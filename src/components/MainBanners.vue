96fce78415ea8379695b10114205536f6d57ad62
Roxana <ms.roxanarahimi@gmail.com>
2026-02-18 18:29:08 +0330

    shop

--- src/components/MainBanners.vue
+++ src/components/MainBanners.vue
@@ -1,14 +1,14 @@
 <template>
 <div class="row p-3 text-light banners" >
-  <div class="col-md-8 rounded justify-content-between row m-0 banner1 mb-1 mb-md-0">
+  <div class="col-md-8 border-radius-2 justify-content-between row m-0 banner1 mb-1 mb-md-0">
     <div class="col-6 col-md-6 text-light px-0 d-grid my-md-5">
       <div class="align-self-center px-md-3">
         <p class="fw-bolder text-center mb-2 mb-md-4 banner1-head">به فروشگاه rx خوش آمدید</p>
         <p class="fw-bolder text-center mb-2 banner1-txt" style="">بهترن و جدیدترین محصولات روز دنیا
           <br class="d-none d-md-block">
           با ضمانت اورجینال بودن و بهترین قیمت</p>
-        <router-link to="/shop" class="btn btn-lg d-none d-md-block btn-primary d-block mt-5">برو به فروشگاه</router-link>
-        <router-link to="/shop" class="btn btn-sm d-md-none btn-primary d-block">برو به فروشگاه</router-link>
+        <router-link to="/shop" class="btn btn-lg d-none d-md-block btn-primary border-radius-2  d-block mt-5">برو به فروشگاه</router-link>
+        <router-link to="/shop" class="btn btn-sm d-md-none btn-primary border-radius-2  d-block">برو به فروشگاه</router-link>
       </div>
     </div>
     <div class="col-6 p-0 px-md-2 col-md-6 d-grid ">
@@ -17,7 +17,7 @@
   </div>
   <div class="col-md-4 row p-0 px-md-3 m-0">
     <div class="col-12 col-md-12 mb-1 mb-md-3 ">
-      <div class=" banner2 rounded row justify-content-between">
+      <div class=" banner2 border-radius-2 row justify-content-between">
         <div class="col-6 col-md-5 d-grid">
           <h3 class="align-self-center text-center">محصولات نامبوزین شارژ شد بدو!</h3>
         </div>
@@ -27,7 +27,7 @@
       </div>
     </div>
     <div class="col-12 col-md-12 ">
-      <div class="banner3 rounded row justify-content-between">
+      <div class="banner3 border-radius-2 row justify-content-between">
         <div class="col-6 col-md-5 d-grid">
           <h3 class="align-self-center text-center">رژ لب جدیدای شیگلم رو دیدی؟؟</h3>
         </div>
