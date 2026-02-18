ca19890ab38f91315f5dc2be5fad05ca6bf72c26
Roxana <ms.roxanarahimi@gmail.com>
2026-02-18 20:39:20 +0330

    shop

--- src/components/MainBanners.vue
+++ src/components/MainBanners.vue
@@ -2,11 +2,11 @@
 <div class="row p-3 text-light banners" >
   <div class="col-md-8 border-radius-2 justify-content-between row m-0 banner1 mb-1 mb-md-0">
     <div class="col-6 col-md-6 text-light px-0 d-grid my-md-5">
-      <div class="align-self-center px-md-3">
-        <p class="fw-bolder text-center mb-2 mb-md-4 banner1-head">به فروشگاه rx خوش آمدید</p>
-        <p class="fw-bolder text-center mb-2 banner1-txt" style="">بهترن و جدیدترین محصولات روز دنیا
+      <div class="align-self-center px-0 px-md-3">
+        <p class="fw-bolder  mb-2 mb-md-4 banner1-head">به فروشگاه rx خوش آمدید</p>
+        <p class="fw-bolder  mb-2 banner1-txt" style="">بهترین و جدیدترین محصــولات روز دنیـــا
           <br class="d-none d-md-block">
-          با ضمانت اورجینال بودن و بهترین قیمت</p>
+          با ضمـــانت اورجیـــنال بودن و بهترین قیمت</p>
         <router-link to="/shop" class="btn btn-lg d-none d-md-block btn-primary border-radius-2  d-block mt-5">برو به فروشگاه</router-link>
         <router-link to="/shop" class="btn btn-sm d-md-none btn-primary border-radius-2  d-block">برو به فروشگاه</router-link>
       </div>
@@ -18,8 +18,8 @@
   <div class="col-md-4 row p-0 px-md-3 m-0">
     <div class="col-12 col-md-12 mb-1 mb-md-3 ">
       <div class=" banner2 border-radius-2 row justify-content-between">
-        <div class="col-6 col-md-5 d-grid">
-          <h3 class="align-self-center text-center">محصولات نامبوزین شارژ شد بدو!</h3>
+        <div class="col-6 col-md-6 d-grid">
+          <h4 class="align-self-center  banner-txt2">محصولات نامبوزین شارژ شد بدو!</h4>
         </div>
         <div class="col-5 col-md-6 d-grid justify-content-end">
           <img src="/img/eye.png" class="w-75 d-block align-self-center" alt="">
@@ -28,8 +28,8 @@
     </div>
     <div class="col-12 col-md-12 ">
       <div class="banner3 border-radius-2 row justify-content-between">
-        <div class="col-6 col-md-5 d-grid">
-          <h3 class="align-self-center text-center">رژ لب جدیدای شیگلم رو دیدی؟؟</h3>
+        <div class="col-6 col-md-6 d-grid">
+          <h4 class="align-self-center  banner-txt2">رژ لب جدیدای شیگلم رو دیدی؟؟</h4>
         </div>
         <div class="col-5 col-md-6 d-grid justify-content-end">
           <img src="/img/ss.png" class="w-75 align-self-center" alt="">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <template>
-<!--  <main-carousel/>-->
-<main-banners />
-  <div class=" text-center row p-1 pt-3 pb-0 bg-primary"  style="color: white; ">
+  <!--  <main-carousel/>-->
+  <main-banners/>
+  <div class=" text-center row p-1 pt-3 pb-0 bg-primary" style="color: white; ">
     <div class="col-3 p-0 col-md-3 mb-3">
       <i class="bi bi-patch-check d-block fs-big"></i>
       <small class="fs-small">100% اورجینال</small>
     </div>
-     <div class="col-3 p-0 col-md-3 mb-3">
+    <div class="col-3 p-0 col-md-3 mb-3">
       <i class="bi bi-star d-block fs-big"></i>
       <small class="fs-small">محصولات ترند</small>
     </div>
@@ -20,31 +20,54 @@
     </div>
   </div>
 
-  <div class="row bg-light pt-md-5 ">
-    <div class="col-lg-10 mx-auto row px-5 py-4 py-md-0">
-      <div v-for="item in categories" class="col-4  col-md-4 p-3 p-md-4">
+  <div class="row bg-light pt-md-5  ">
+    <div class="col-lg-10 mx-auto row px-4 px-md-5 py-4 py-md-0  mb-md-5">
+      <div v-for="item in categories" class="col-4 col-md-4 p-4">
         <div class="row bg-white cat-section ">
           <div class="col-md-6 px-0 text-center cat-img-container">
             <img :src="imgUrl+item.image" class="img-fluid cat-img w-100" alt="">
           </div>
           <div class="col-md-6 cat-label d-grid ps-3 ps-md-4">
             <div style="align-self: center">
-              <h5 class="cat-txt fw-bold text-center mt-2 mt-md-0">محصولات  {{ item.title }}</h5>
-<!--              <br><br>-->
-<!--       <small class="mb-0 cat-txt">65 محصول</small>&lt;!&ndash;      &ndash;&gt;-->
+              <h5 class="cat-txt fw-bold text-center mt-2 mt-md-0">محصولات {{ item.title }}</h5>
+              <!--              <br><br>-->
+              <!--       <small class="mb-0 cat-txt">65 محصول</small>&lt;!&ndash;      &ndash;&gt;-->
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-  <new-products-section :cats="categories" :url="url" :imgUrl="imgUrl" />
 
-  <div class="text-center">
-    <router-link to="/shop" class="btn btn-primary d-inline-block px-5 mb-5 mt-5 mt-md-0 border-radius-2 ">فروشگاه</router-link>
+  <div class="row cta justify-content-center">
+   <div class="row  justify-content-between col-md-8">
+     <div class="col-md-5 d-grid">
+       <div class="align-self-center p-5 pb-0 p-md-0">
+         <h2 class="cta-txt text-light">
+           برای گرفتن کدتخفیف اولین
+           <br>
+           خریدت همین الان عضو شو!
+         </h2>
+         <div class="align-self-center d-flex justify-content-md-end mb-5 ps-5">
+           <router-link class="btn btn-success border-radius-2 text-light fw-bold px-5" to="/login">ثبت نام</router-link>
+         </div>
+       </div>
+     </div>
+
+     <div class="col-md-6 mt-md-5 pt-md-5">
+       <img src="/img/gg.png" class="w-100" alt="">
+     </div>
+   </div>
   </div>
 
-  <div class=" d-flex flex-wrap justify-content-between pt-4 " style="background-color: rgba(230,0,118,0.15)">
+  <new-products-section :cats="categories" :url="url" :imgUrl="imgUrl"/>
+
+  <div class="text-center">
+    <router-link to="/shop" class="btn btn-primary d-inline-block px-5 mb-5 mt-5 mt-md-0 border-radius-2 ">فروشگاه
+    </router-link>
+  </div>
+
+  <div class=" d-flex flex-wrap justify-content-between px-3 pt-4 brands-wrapper " style="background-color: #ffaad5">
     <div class="brand"><img src="/img/brands/cosrx.jpg" class="w-100" alt=""></div>
     <div class="brand"><img src="/img/brands/phyto.jpg" class="w-100" alt=""></div>
     <div class="brand"><img src="/img/brands/sheglam.png" class="w-100" alt=""></div>
@@ -67,23 +90,23 @@ import App from "@/App.vue";
 export default {
   name: 'HomeView',
   components: {mainBanners, newProductsSection,},
-  setup(){
+  setup() {
     const categories = ref([]);
-    const getCats = ()=>{
-      axios.get(url+'/api/categories')
-          .then((response)=>{
+    const getCats = () => {
+      axios.get(url + '/api/categories')
+          .then((response) => {
             categories.value = response.data;
           })
     }
     const url = App.setup().url;
     const imgUrl = App.setup().imgUrl;
 
-    onBeforeMount(()=>{
+    onBeforeMount(() => {
       getCats();
       console.log(categories.value)
     })
 
-    return{
+    return {
       url, imgUrl, categories, getCats
     }
   }
