7ba752708b64be990d5deeb67366f951ea4b47a9
Roxana <ms.roxanarahimi@gmail.com>
2026-09-25 21:29:42 +0330

    shop

--- src/views/Verification.vue
+++ src/views/Verification.vue
@@ -18,8 +18,10 @@ export default {
 
     const verifyPayment = ()=>{
       axios.post(url + '/api/verify/payment',{
+        query: {
           Authority: authority,
           Status: status
+        }
       }).then((response)=>{
 
       }).catch((error)=>{
