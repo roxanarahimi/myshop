52229cbb44a7ddf114a47e16cd1228637a0b3028
Roxana <ms.roxanarahimi@gmail.com>
2026-09-26 00:01:40 +0330

    shop

--- src/views/Verification.vue
+++ src/views/Verification.vue
@@ -1,6 +1,19 @@
 <template>
 <div>
-  hhiihihi
+  <div class="row d-grid vh-100">
+    <div class="col-md-8 col-lg-5 mx-auto align-self-center">
+     <div class="card w-100 border border-dashed">
+       <div class="card-body">
+         <h3 class="w-100 text-center">{{result.title}}</h3>
+         <b class="w-100 text-center">{{result.message}}</b>
+         <div class="d-flex justify-content-between"><b>شماره سفارش</b><b>{{result.code}}</b></div>
+         <div class="d-flex justify-content-between"><b>پرداخت شما</b><b>{{result.amount}}</b></div>
+         <div class="d-flex justify-content-between"><b>کد پیگیری تراکنش</b><b>{{result.referenceId}}</b></div>
+       </div>
+     </div>
+    </div>
+
+  </div>
 </div>
 </template>
 
@@ -15,6 +28,8 @@ export default {
     const url = App.setup().url;
     const authority = route.query.Authority
     const status = route.query.Status
+    const order_id = route.query.oid
+    const result = ref({})
 
     const verifyPayment = ()=>{
       axios.post(url + '/api/verify/payment',
@@ -22,11 +37,16 @@ export default {
           {
             params: {
               Authority: authority,
-              Status: status
+              Status: status,
+              order_id: order_id
             }
 
       }).then((response)=>{
-
+        if(response.status === 200){
+          result.value = response.data
+        }else{
+          result.value = {message:'پرداخت ناموفق'}
+        }
       }).catch((error)=>{
         console.error(error)
       })
@@ -39,7 +59,7 @@ export default {
     })
 
     return{
-      route, verifyPayment, authority,status
+      route, verifyPayment, authority,status,order_id,result
     }
   }
 }
