30312c6706524492786dce48a0bf60e844816bc9
Roxana <ms.roxanarahimi@gmail.com>
2026-09-26 11:14:18 +0330

    shop

--- src/views/Verification.vue
+++ src/views/Verification.vue
@@ -57,7 +57,7 @@ export default {
     onMounted(()=>{
       console.log(route)
       if (status === 'NOK'){
-        result.value.message = 'پرداخت انجام نشد'
+        result.value = {message:'پرداخت انجام نشد'}
       }
       verifyPayment();
     })
